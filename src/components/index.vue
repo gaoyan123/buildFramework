9ac83962d69e348dbe2796a3627daedea7fdc090
gy <982008488@qq.com>
2019-05-21 17:50:11 +0800

    提交Vue -cli3.0框架，集成vue-router

---
+++ src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return {
+                msg:'hello'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
